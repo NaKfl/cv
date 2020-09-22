97eaec4007ad39ab939b404dac201f3c1359dd3d
Nguyen Huu Gia Tri <nguyenhuugiatri@gmail.com>
2020-09-22 22:33:09 +0700

    [1.0.0] Create dynamic background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          width: '100%',
+          height: '100vh',
+          background:
+            'linear-gradient(to bottom right, #48b1bf 0%, #0856c1 100%)',
+        }}
+      >
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 100,
+              },
+              size: {
+                value: 3,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+              },
+            },
+          }}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
