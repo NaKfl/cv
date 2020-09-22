a0047db06aed455195294c729f59658bc352219f
Nguyen Huu Gia Tri <nguyenhuugiatri@gmail.com>
2020-09-22 22:34:18 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.424bbbd93a8814ca928eac570e83d1f1.js"
+  "/personal-website/precache-manifest.69a747eca2716664c699bbab634e6cec.js"
 );
 
 self.addEventListener('message', (event) => {
