2e1ff37d549042a32b586ff820e482a3cede06eb
Nguyen Huu Gia Tri <nguyenhuugiatri@gmail.com>
2020-09-28 16:59:37 +0700

    [1.0.0] Dynamic render for works card

--- src/components/Card/Content/Works/index.js
+++ src/components/Card/Content/Works/index.js
@@ -1,48 +1,70 @@
+import classnames from 'classnames';
 import React, { Component } from 'react';
 import Title from '../../../Title';
 import './styles.scss';
 
+const works = [
+  {
+    time: '11/2019 - 03/2020',
+    company: 'Hybrid Technologies',
+    position: 'Javascript Intern',
+    experiences: [
+      'Participate in software development activities following Scrum process',
+      'Implement software features or fix bugs',
+      'Write unit tests and documents',
+    ],
+  },
+  {
+    time: '03/2020 - Now',
+    company: 'University of Science',
+    position: 'Senior student',
+    experiences: [
+      'Study advanced subjects in web in particular and software in general',
+      'Develop and deploy personal projects',
+      'Improve English skills',
+    ],
+  },
+];
 class ContentWorks extends Component {
+  renderList = (works) => {
+    return works.map((item, index, array) => {
+      return (
+        <div
+          className={classnames('works__content__timeline__single', {
+            works__content__timeline__head: index === 0,
+            works__content__timeline__tail: index === array.length - 1,
+          })}
+        >
+          <div className='works__content__timeline__single__time'>
+            <p>{item.time}</p>
+            <span>{item.company}</span>
+          </div>
+          <div className='works__content__timeline__single__info'>
+            <h4>{item.position}</h4>
+            <p>
+              {item.experiences.map((experience, pos, arr) => {
+                return (
+                  (pos !== arr.length - 1 && (
+                    <>
+                      &sdot; {experience} <br />
+                    </>
+                  )) || <>&sdot; {experience}</>
+                );
+              })}
+            </p>
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
     return (
       <div className='works'>
         <Title>Experience</Title>
         <div className='works__content'>
           <div className='works__content__timeline'>
-            <div className='works__content__timeline__single works__content__timeline__head'>
-              <div className='works__content__timeline__single__time'>
-                <p>11/2019 - 03/2020</p>
-                <span>Hybrid Technologies</span>
-              </div>
-              <div className='works__content__timeline__single__info'>
-                <h4>Javascript Intern</h4>
-                <p>
-                  &sdot; Participate in software development activities
-                  following Scrum process
-                  <br />
-                  &sdot; Implement software features or fix bugs
-                  <br />
-                  &sdot; Write unit tests and documents
-                </p>
-              </div>
-            </div>
-            <div className='works__content__timeline__single works__content__timeline__tail'>
-              <div className='works__content__timeline__single__time'>
-                <p>03/2020 - Now</p>
-                <span>University of Science</span>
-              </div>
-              <div className='works__content__timeline__single__info'>
-                <h4>Senior student</h4>
-                <p>
-                  &sdot; Study advanced subjects in web in particular and
-                  software in general
-                  <br />
-                  &sdot; Develop and deploy personal projects
-                  <br />
-                  &sdot; Improve English skills
-                </p>
-              </div>
-            </div>
+            {this.renderList(works)}
           </div>
         </div>
       </div>
