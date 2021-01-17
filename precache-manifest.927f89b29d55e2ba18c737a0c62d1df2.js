758fa8f386578d16e08d6ca91fdc4200ecee739d
Lem <nguyenhuugiatri@gmail.com>
2021-01-17 23:28:52 +0700

    Updates

---
+++ precache-manifest.927f89b29d55e2ba18c737a0c62d1df2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd568b7fdd1fcb8b50f8efeee2031b26",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "367d809443799001f5f8",
+    "url": "/cv/static/css/main.6e3ed8ea.chunk.css"
+  },
+  {
+    "revision": "6bc80dc32e41c113725e",
+    "url": "/cv/static/js/2.e17cb798.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/cv/static/js/2.e17cb798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367d809443799001f5f8",
+    "url": "/cv/static/js/main.817ece8c.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  },
+  {
+    "revision": "c924155285cdba2f57cbd751674c1bb0",
+    "url": "/cv/static/media/antd.c9241552.png"
+  },
+  {
+    "revision": "c80e96307ce0a09b77a42bf87da3c7e7",
+    "url": "/cv/static/media/avatar.c80e9630.jpg"
+  },
+  {
+    "revision": "328bcc064b92b245ce2c75fb08db38d0",
+    "url": "/cv/static/media/logo.328bcc06.png"
+  },
+  {
+    "revision": "99936cc095f5e9b0edec81fb3f51f753",
+    "url": "/cv/static/media/thank.99936cc0.gif"
+  }
+]);
